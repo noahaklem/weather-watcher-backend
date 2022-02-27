72b9b68706845e541c78e0b841335eb9329c2880
Noah A Klem <noahaklem@gmail.com>
2022-02-26 17:05:02 -0700

    initialize a location with coordinates services results

--- app/poros/location.rb
+++ app/poros/location.rb
@@ -1,3 +1,8 @@
 class Location 
-
+  attr_reader :latitude, :longitude
+  # take results from coordinates_service and initialize a location instance
+  def initialize(data)
+    @latitude = data[:lat]
+    @longitude = data[:lng]
+  end
 end
