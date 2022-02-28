f2968eafca73ec460fcb1d5e7fec5e2557a8c224
Noah A Klem <noahaklem@gmail.com>
2022-02-27 17:33:53 -0700

    new forecast instance from facade

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -2,9 +2,7 @@ class ForecastFacade
   def self.get_forecast(input)
     coordinates = CoordinatesService.get_coordinates(input)
     location = Location.new(input, coordinates)
-    weather = WeatherService.get_forecast(location)
+    Forecast.new(WeatherService.get_forecast(location))
     debugger
-
-    #will need to send the information from weather into a forecast model
   end
 end
