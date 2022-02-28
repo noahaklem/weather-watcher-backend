3a72ef9965c5fd775ec223b9ba4046a857cb70fe
Noah A Klem <noahaklem@gmail.com>
2022-02-27 17:28:59 -0700

    forgot get_forecast method with WeatherService call

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -2,7 +2,7 @@ class ForecastFacade
   def self.get_forecast(input)
     coordinates = CoordinatesService.get_coordinates(input)
     location = Location.new(input, coordinates)
-    weather = WeatherService(location)
+    weather = WeatherService.get_forecast(location)
     debugger
 
     #will need to send the information from weather into a forecast model
