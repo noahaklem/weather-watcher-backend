7e9221c04300309a872d9e8711bc0fc316116d41
Noah A Klem <noahaklem@gmail.com>
2022-03-06 19:52:53 -0700

    refactor notes

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::AuthController < ApplicationController
-  skip_before_action :authorized, only: [:create]
+  # skip_before_action :authorized, only: [:create]
 
   def create
+    byebug
     user = User.find_by(username: user_login_params[:username])
 
     if user && user.authenticate(user_login_params[:password])
