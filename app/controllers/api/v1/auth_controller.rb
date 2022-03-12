63bb15d5ff6d29170ba7af984ab45c08bd4d121b
Noah A Klem <noahaklem@gmail.com>
2022-03-11 21:57:04 -0700

    auth adds

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::AuthController < ApplicationController
   # skip_before_action :authorized, only: [:create]
 
   def create
-    byebug
     user = User.find_by(username: user_login_params[:username])
 
     if user && user.authenticate(user_login_params[:password])
